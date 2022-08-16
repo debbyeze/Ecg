734646b22e45585e1df1154e0c2d1e105f050652
debbyeze <deborahsong7@gmail.com>
2022-08-16 07:51:18 +0100

    first commit

---
+++ pages/addpatient.js
@@ -0,0 +1,11 @@
+var loadFile = function(event){
+  var image = document.getElementById('output');
+  image.style.display="block";
+  image.src = URL.createObjectURL(event.target.files[0]);
+  document.getElementById('selected').style.display= "block";
+  };
+
+   function validateForm(){
+  alert("New patient added!");
+  };
+   
