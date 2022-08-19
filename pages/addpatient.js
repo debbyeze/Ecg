5227d8519c0191efc3d033d143e0bb7f45f2f547
debbyeze <deborahsong7@gmail.com>
2022-08-19 16:02:25 +0100

    modal

--- pages/addpatient.js
+++ pages/addpatient.js
@@ -2,10 +2,27 @@ var loadFile = function(event){
   var image = document.getElementById('output');
   image.style.display="block";
   image.src = URL.createObjectURL(event.target.files[0]);
-  document.getElementById('selected').style.display= "block";
+  document.getElementById('photofield').style.width= "150px";
+  document.getElementById('photolegend').style.padding="0px";
+  
   };
 
-   function validateForm(){
-  alert("New patient added!");
-  };
+
+
+  
+function validateForm(){
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+  modal.style.display = "block";
+[10000]}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
    
